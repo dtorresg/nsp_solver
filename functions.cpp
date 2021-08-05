85a2b577e59caceac55944685493ab1bf76c4171
dtorresg <daniel.torresg@sansano.usm.cl>
2021-08-05 16:12:22 -0400

    Correccion

--- functions.cpp
+++ functions.cpp
@@ -241,7 +241,7 @@ int eval_func(int days, vector<SecShift> SECTION_SHIFTS, vector<vector<vector<in
             }
         }
         if (b_e[i] > temp){
-            suma3 = suma3 + (b_e[i] - temp)*500;
+            suma3 = suma3 + (b_e[i] - temp)*1000;
         }
         temp = 0;
     }
@@ -265,7 +265,7 @@ int eval_func(int days, vector<SecShift> SECTION_SHIFTS, vector<vector<vector<in
                 temp++;
             }else{
                 if (temp < f_e[i] && temp != 0){
-                    suma5 = suma5 + (f_e[i] - temp)*500;
+                    suma5 = suma5 + (f_e[i] - temp)*1000;
                 }
                 temp = 0;
             }
